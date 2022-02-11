e9d1e1ded150d9315d4ca1aba3e698cf0f18e8f5
Leandro Zanardi <lezanardi@gmail.com>
2022-02-11 07:59:40 -0300

    gmaps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,29 @@
   <main class="home" >
 
     <div class="left">
-      de: <input type="text" style="border:1px solid;"/><br/>
-      para: <input type="text"  style="border:1px solid;"/>
+      <GMapAutocomplete
+        placeholder="Digite o ponto de origem"
+        @place_changed="setPlace"
+        class="autocomplete"
+      >
+      </GMapAutocomplete>
+
+
+      <GMapAutocomplete
+        placeholder="Digite o ponto de destino"
+        @place_changed="setPlace"
+        class="autocomplete"
+      >
+      </GMapAutocomplete>
+
+      <button class="botao_cotar"> Cotar </button>
     </div>
 
     <div class="right">
-
+      <GMapMap
+        :center="{lat: -23.6815302, lng: -46.8761758}"
+        :zoom="17" style="height:600px;"
+      />
     </div>
 
     
@@ -17,27 +34,44 @@
 
 <script setup>
   
-  import { ref } from 'vue'
+  //import { ref } from 'vue'
 
-  const texto = ref('');
-  texto.value = "bem vindos";
+  function setPlace(param1) {
+    console.log(param1)
+  }
 
-  // function alertar() {
-  //   console.log(texto);
-  //   texto.value = "teste";
-  // }
 
 </script>
 
 <style scoped>
 .home {
+  height: 600px;
+}
+
+.vue-map-container {
+  height:600px;
+}
+
+.botao_cotar {
+  width:200px;
+  height:50px;
+}
+
+.autocomplete {
+  border:1px solid;
 }
 
 .left{
   width:50%;
+  height:calc(100%);
+  float:left;
+  background-color: beige;
 }
 
 .right {
+  background-color: aquamarine;
   width:50%;
+  height:600px;
+  float:right;
 }
 </style>
