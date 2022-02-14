009cec123b248b9c48bfb48d8585dfe9e0f7ce2f
Leandro Zanardi <lezanardi@gmail.com>
2022-02-14 07:57:57 -0300

    pin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,9 +22,17 @@
 
     <div class="right">
       <GMapMap
-        :center="{lat: -23.6815302, lng: -46.8761758}"
-        :zoom="17" style="height:600px;"
-      />
+        :center="mapCenter"
+        :zoom="16" style="height:600px;"
+      >
+        <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+        />
+      </GMapMap>
     </div>
 
     
@@ -34,10 +42,34 @@
 
 <script setup>
   
-  //import { ref } from 'vue'
+  import { ref } from 'vue';
+
+  const mapCenter = ref({});
+  mapCenter.value = {lat: -23.6815302, lng: -46.8761758};
+
+  const markers = ref({});
+  
+  markers.value = [];
+
+  // {
+  //   position: {
+  //     lat: 51.093048, lng: 6.842120
+  //   },
+  // }
+
 
   function setPlace(param1) {
     console.log(param1)
+    console.log(param1.geometry.location.lat())
+    console.log(param1.geometry.location.lng())
+
+    let point = { lat: param1.geometry.location.lat(), lng: param1.geometry.location.lng() };
+
+    mapCenter.value = point;
+
+    markers.value.push({
+      position: point
+    })
   }
 
 
