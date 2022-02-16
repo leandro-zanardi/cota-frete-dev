9563a40e64b4f1fe3cddfe24977df374513536f4
Leandro Zanardi <lezanardi@gmail.com>
2022-02-16 07:44:57 -0300

    add plugin

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
   box-sizing: border-box;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: right;
