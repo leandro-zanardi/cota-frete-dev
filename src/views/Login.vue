a2cbc4b7a2b6467668b60c19fbf257d472a9aec5
Leandro Zanardi <lezanardi@gmail.com>
2022-02-17 08:05:04 -0300

    flex box

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,22 @@
 <template>
-	<MDBRow>
-		<MDBCol sm="12" lg="6">
-			<form @submit.prevent="register">
+	<div class="h-100 d-flex justify-content-around align-items-center">
+			<form @submit.prevent="register" class="register flex-fill p-3">
 				<h2>Crie sua conta</h2>
 				<MDBInput
 					size="lg"
 					type="email" 
 					label="Insira seu email" 
-					v-model="register_form.email" 
+					v-model="register_form.email"
+					wrapperClass="mb-3"
 				/>
 				
 				<MDBInput
 					size="lg"
 					type="password" 
 					label="Crie uma senha" 
-					v-model="register_form.password" />
+					v-model="register_form.password"
+					wrapperClass="mb-3"
+					/>
 				<MDBBtn
 					color="primary"
 					type="submit"
@@ -23,33 +25,32 @@
 					Cadastrar
 				</MDBBtn>
 			</form>
-		</MDBCol>
-		<MDBCol sm="12" lg="6">
-			<form class="login" @submit.prevent="login">
+	
+			<form class="login flex-fill  p-3" @submit.prevent="login">
 				<h2>Acesse sua conta</h2>
-				<input 
+				<MDBInput
+					size="lg"
+					wrapperClass="mb-3"
 					type="email" 
 					placeholder="Digite seu email"
 					v-model="login_form.email" />
-				<input 
+				<MDBInput
+					size="lg"
+					wrapperClass="mb-3"
 					type="password" 
 					placeholder="Digite sua senha" 
 					v-model="login_form.password" />
-				<MDBBtn color="primary" type="submit">
+				<MDBBtn color="primary" type="submit" size="lg">
 					Entrar
 				</MDBBtn>
 			</form>
-		</MDBCol>
-	</MDBRow>
-
+	</div>
 </template>
 
 
 <script setup>
 	import { ref } from 'vue'
 	import { useStore } from 'vuex'
-	import { MDBRow } from "mdb-vue-ui-kit";
-	import { MDBCol } from "mdb-vue-ui-kit";
 	import { MDBBtn } from "mdb-vue-ui-kit";
 	import { MDBInput } from "mdb-vue-ui-kit";
 
@@ -74,6 +75,13 @@ form.login {
 	background-position: bottom;
 	background-clip: content-box;
 	background-size: 25rem;
+	max-height:450px;
+	min-height: 400px;
+}
+
+form.register {
+	max-height:450px;
+	min-height: 400px;
 }
          
 </style>
