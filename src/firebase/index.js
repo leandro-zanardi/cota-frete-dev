165f1920e97b0dd8cc87d7f05ae29c68fb29193c
Leandro Zanardi <lezanardi@gmail.com>
2022-02-22 07:07:29 -0300

    firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -12,18 +12,19 @@ import { getDatabase, ref, set, onValue } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ0S7igB2f6WOA3S24Bs9jy572yix8W7k",
-  authDomain: "cota-frete-dev.firebaseapp.com",
-  projectId: "cota-frete-dev",
-  storageBucket: "cota-frete-dev.appspot.com",
-  messagingSenderId: "649445319498",
-  appId: "1:649445319498:web:368c23fd2f33e8909b57b3",
-  measurementId: "G-T1K7S4B491"
+  apiKey: "AIzaSyB9IoZp3K67MELZQOZQweGyKWin9q7lUb8",
+  authDomain: "cota-frete-estudo.firebaseapp.com",
+  databaseURL: "https://cota-frete-estudo-default-rtdb.firebaseio.com",
+  projectId: "cota-frete-estudo",
+  storageBucket: "cota-frete-estudo.appspot.com",
+  messagingSenderId: "1041288545392",
+  appId: "1:1041288545392:web:4242cad821b19f5a25a0b5",
+  measurementId: "G-FR08559ES6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firebaseDB = getDatabase(app, "https://cota-frete-dev-default-rtdb.firebaseio.com/")
+const firebaseDB = getDatabase(app, "https://cota-frete-estudo-default-rtdb.firebaseio.com/")
 //const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
