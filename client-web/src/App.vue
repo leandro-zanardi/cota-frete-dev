858613f177aba08b06f0a774224325eefb870688
rafael-mm <rafael.men.maciel@gmail.com>
2022-04-11 14:59:12 -0300

    alterações da navbar

--- client-web/src/App.vue
+++ client-web/src/App.vue
@@ -3,12 +3,12 @@
   <div class="h-100 d-flex flex-column">
     <div id="nav" v-if="$store.state.userStore.user">
 
-    <MDBNavbar light bg="light" expand="md" container>
+    <MDBNavbar light style="background-color: #00BCD4" expand="md" container>
         <MDBNavbarNav class="d-flex flex-row" center> 
           <MDBNavbarItem active><router-link class="p-2" to ="/">Home</router-link> | </MDBNavbarItem>
           <MDBNavbarItem active><router-link class="p-2" to ="/">About</router-link></MDBNavbarItem>
           <MDBNavbarItem v-show="$store.state.userStore.isAdmin"><router-link class="p-2" to ="/fornecedores">Fornecedores</router-link></MDBNavbarItem>
-          <MDBNavbarItem active><MDBBtn size="sm" outline="primary" @click="$store.dispatch('userStore/logout')">Logout</MDBBtn></MDBNavbarItem>
+          <MDBNavbarItem active><MDBBtn size="sm" outline="light" @click="$store.dispatch('userStore/logout')">Logout</MDBBtn></MDBNavbarItem>
         </MDBNavbarNav>
       </MDBNavbar>
 
@@ -44,5 +44,29 @@ html, body {
 #app {
   height: calc(100%);
   font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+
+a:link {
+  color: green;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:visited {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
 }
 </style>
