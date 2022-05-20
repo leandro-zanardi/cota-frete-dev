64ab5af84fc089ec922eb1642224a3f8408bb19c
Rafael Maciel <rafael.men.maciel@gmail.com>
2022-05-20 00:06:43 -0300

    GridView da Configuração de Usuário (incompleto)

--- app/lib/views/configuracao_usuario_view.dart
+++ app/lib/views/configuracao_usuario_view.dart
@@ -18,74 +18,86 @@ class ConfiguracaoUsuarioView extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Configurações'),
         ),
-        body: Stack(children: [
-          Column(
-            children: [
-              const Text('Configurações de Usuário',
-                  style: TextStyle(
-                    height: 3,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                  )),
-              Observer(
-                builder: (_) {
-                  return GridView.count(crossAxisCount: 2, children: [
-                    Row(
-                      children: const [Text("Email"), Text("Ações")],
-                    ),
-                    for (UserClient client in adminStore.userClients)
-                      Row(
-                        children: [
-                          Text(client.email),
-                          ElevatedButton(
-                            onPressed: () => adminStore.registerUserAdmin(
-                                client.uuid, !client.admin),
-                            child: Text(client.admin
-                                ? "Desativar Admin"
-                                : "Tornar Admin"),
-                            style: ElevatedButton.styleFrom(
-                                primary: (client.admin
-                                    ? const Color.fromARGB(255, 147, 42, 34)
-                                    : const Color.fromARGB(255, 74, 70, 186))),
-                          )
-                        ],
-                      )
-                  ]);
-                },
-              )
-            ],
-          ),
-          Observer(
-              builder: (_) => Visibility(
-                  visible: adminStore.loading,
-                  child: Stack(children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      color: Colors.black45,
-                    ),
-                    Center(
-                        child: Container(
-                            width: MediaQuery.of(context).size.width * 0.7,
-                            height: MediaQuery.of(context).size.height * 0.3,
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(),
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(20))),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                mainAxisSize: MainAxisSize.min,
-                                children: const [
-                                  Text("Carregando...",
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold)),
-                                  Center(child: CircularProgressIndicator()),
-                                ]))),
-                  ])))
-        ]));
+        body: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child:
+                // Stack(children: [
+                //   Column(
+                //     children: [
+                //       const Text('Configurações de Usuário',
+                //           style: TextStyle(
+                //             height: 3,
+                //             fontSize: 30,
+                //             fontWeight: FontWeight.w500,
+                //           )),
+                //       Observer(
+                //         builder: (_) {
+                //           return
+                GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 3,
+              children: [
+                const Text("E-mail",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const Text("Ações",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                for (UserClient client in adminStore.userClients)
+                  Column(children: [
+                    Text(client.email),
+                    ElevatedButton(
+                      onPressed: () => adminStore.registerUserAdmin(
+                          client.uuid, !client.admin),
+                      child: Text(
+                          client.admin ? "Desativar Admin" : "Tornar Admin"),
+                      style: ElevatedButton.styleFrom(
+                          primary: (client.admin
+                              ? const Color.fromARGB(255, 147, 42, 34)
+                              : const Color.fromARGB(255, 74, 70, 186))),
+                    )
+                  ])
+              ],
+            ))
+        //           ;
+        //         },
+        //       )
+        //     ],
+        //   ),
+        //   Observer(
+        //       builder: (_) => Visibility(
+        //           visible: adminStore.loading,
+        //           child: Stack(children: [
+        //             Container(
+        //               width: MediaQuery.of(context).size.width,
+        //               height: MediaQuery.of(context).size.height,
+        //               color: Colors.black45,
+        //             ),
+        //             Center(
+        //                 child: Container(
+        //                     width: MediaQuery.of(context).size.width * 0.7,
+        //                     height: MediaQuery.of(context).size.height * 0.3,
+        //                     decoration: BoxDecoration(
+        //                         color: Colors.white,
+        //                         border: Border.all(),
+        //                         borderRadius: const BorderRadius.all(
+        //                             Radius.circular(20))),
+        //                     child: Column(
+        //                         crossAxisAlignment: CrossAxisAlignment.center,
+        //                         mainAxisAlignment:
+        //                             MainAxisAlignment.spaceEvenly,
+        //                         mainAxisSize: MainAxisSize.min,
+        //                         children: const [
+        //                           Text("Carregando...",
+        //                               style: TextStyle(
+        //                                   fontSize: 14,
+        //                                   fontWeight: FontWeight.bold)),
+        //                           Center(child: CircularProgressIndicator()),
+        //                         ]))),
+        //           ])))
+        // ])
+        );
   }
 }
