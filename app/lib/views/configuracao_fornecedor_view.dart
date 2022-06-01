1394db6120920a9480fa951f31461edf2538c933
Rafael Maciel <rafael.men.maciel@gmail.com>
2022-05-31 22:36:16 -0300

    ideia de tabela para visualização de origem-destino

--- app/lib/views/configuracao_fornecedor_view.dart
+++ app/lib/views/configuracao_fornecedor_view.dart
@@ -10,36 +10,25 @@ final FornecedorStore fornecedorStore = GetIt.I.get<FornecedorStore>();
 class ConfiguracaoFornecedorView extends StatelessWidget {
   const ConfiguracaoFornecedorView({Key? key}) : super(key: key);
 
-  List<Widget> buildExpansionTileFornecedores() {
-    List<Widget> expansionTiles = [];
-    List<Widget> listaFornecedores =
-        buildListaFornecedores(fornecedorStore.fornecedores.toList());
-    for (int x = 0; x < listaFornecedores.length; x++) {
-      expansionTiles.add(ExpansionTile(
-          title: const Text('Nome do fornecedor'),
-          children: [listaFornecedores[x]]));
-    }
-    return expansionTiles;
-  }
-
   List<Widget> buildListaFornecedores(List<FornecedorModel> fornecedores) {
     List<Widget> widgets = [];
 
     for (int x = 0; x < fornecedores.length; x++) {
-      Widget w = Column(
-        children: [
+      Widget w = ExpansionTile(
+        title: Column(children: [
           InkWell(
               onTap: () => GetIt.I.get<AppRouter>().pushNamed(
                   "/registro-fornecedor?id_fornecedor=${fornecedores[x].nome}"),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(fornecedores[x].nome),
-              )),
+              ))
+        ]),
+        children: [
           for (int y = 0; y < fornecedores[x].origens.length; y++)
             Padding(
-              padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
-              child: Column(
-                children: [
+                padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
+                child: Column(children: [
                   const Padding(
                     padding: EdgeInsets.fromLTRB(0, 8.0, 0, 8.0),
                     child: Text("Origem"),
@@ -47,44 +36,87 @@ class ConfiguracaoFornecedorView extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      const Text("Capital"),
-                      Text(fornecedores[x].origens[y].capital ? "Sim" : "Não"),
-                      const Text("Estado"),
-                      Text(fornecedores[x].origens[y].estado)
+                      DataTable(columns: const [
+                        DataColumn(label: Text("Estado")),
+                        DataColumn(label: Text("Capital"))
+                      ], rows: [
+                        DataRow(cells: [
+                          DataCell(Text(fornecedores[x].origens[y].estado)),
+                          DataCell(Text(fornecedores[x].origens[y].capital
+                              ? "Sim"
+                              : "Não"))
+                        ])
+                      ]),
+                      // const Text("Capital"),
+                      // Text(fornecedores[x].origens[y].capital ? "Sim" : "Não"),
+                      // const Text("Estado"),
+                      // Text(fornecedores[x].origens[y].estado)
                     ],
                   ),
                   const Text("Destinos"),
-                  for (int z = 0;
-                      z < fornecedores[x].origens[y].destinos.length;
-                      z++)
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 4.0, 0, 4.0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Text("Capital"),
-                            Text(fornecedores[x].origens[y].destinos[z].capital
-                                ? "Sim"
-                                : "Não"),
-                            const Text("Estado"),
-                            Text(fornecedores[x].origens[y].destinos[z].estado),
-                            const Text("Preço por KM"),
-                            Text(fornecedores[x]
+                  DataTable(
+                    columnSpacing: 25,
+                    columns: const [
+                      DataColumn(label: Text("Estado")),
+                      DataColumn(label: Text("Capital")),
+                      DataColumn(label: Text("Preço/Km")),
+                      DataColumn(label: Text("Preço Mín."))
+                    ],
+                    rows: [
+                      for (int z = 0;
+                          z < fornecedores[x].origens[y].destinos.length;
+                          z++)
+                        DataRow(
+                          cells: [
+                            DataCell(Text(
+                                fornecedores[x].origens[y].destinos[z].estado)),
+                            DataCell(Text(
+                                fornecedores[x].origens[y].destinos[z].capital
+                                    ? "Sim"
+                                    : "Não")),
+                            DataCell(Text(fornecedores[x]
                                 .origens[y]
                                 .destinos[z]
                                 .precoKm
-                                .toString()),
-                            const Text("Preço Mínimo"),
-                            Text(fornecedores[x]
+                                .toString())),
+                            DataCell(Text(fornecedores[x]
                                 .origens[y]
                                 .destinos[z]
                                 .precoMin
-                                .toString()),
-                          ]),
-                    ),
-                ],
-              ),
-            )
+                                .toString())),
+                          ],
+                        ),
+                    ],
+                  ),
+                  // for (int z = 0;
+                  //     z < fornecedores[x].origens[y].destinos.length;
+                  //     z++)
+                  //   Padding(
+                  //     padding: const EdgeInsets.fromLTRB(0, 4.0, 0, 4.0),
+                  //     child: Row(
+                  //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //         children: [
+                  //           const Text("Capital"),
+                  //           Text(fornecedores[x].origens[y].destinos[z].capital
+                  //               ? "Sim"
+                  //               : "Não"),
+                  //           const Text("Estado"),
+                  //           Text(fornecedores[x].origens[y].destinos[z].estado),
+                  //           const Text("Preço por KM"),
+                  //           Text(fornecedores[x]
+                  //               .origens[y]
+                  //               .destinos[z]
+                  //               .precoKm
+                  //               .toString()),
+                  //           const Text("Preço Mínimo"),
+                  //           Text(fornecedores[x]
+                  //               .origens[y]
+                  //               .destinos[z]
+                  //               .precoMin
+                  //               .toString()),
+                  //         ]),
+                  //   ),
+                ]))
         ],
       );
       widgets.add(w);
@@ -95,8 +127,6 @@ class ConfiguracaoFornecedorView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     fornecedorStore.getFornecedores();
-    // List<Widget> listaFornecedores =
-    //     buildListaFornecedores(fornecedorStore.fornecedores.toList());
 
     return Scaffold(
         appBar: AppBar(
@@ -120,9 +150,8 @@ class ConfiguracaoFornecedorView extends StatelessWidget {
                         .pushNamed("/registro-fornecedor"),
                     child: const Text("Cadastrar Novo Fornecedor"),
                   ),
-                  Column(children: buildExpansionTileFornecedores()),
-                  // ...buildListaFornecedores(
-                  //     fornecedorStore.fornecedores.toList())
+                  ...buildListaFornecedores(
+                      fornecedorStore.fornecedores.toList()),
                 ],
               ),
             ),
